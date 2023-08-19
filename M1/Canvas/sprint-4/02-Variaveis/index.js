36ef145aaa10a1452e154bd51bc22e24880f64e0
Paloma Cristina Faria <palomacfaria@outlook.com>
2023-08-19 17:09:28 -0300

    Variáveis JS

---
+++ M1/Canvas/sprint-4/02-Variaveis/index.js
@@ -0,0 +1,11 @@
+console.log("Eu funciono!");
+console.log("Estou no HTML arquivo de script interno.");
+
+let meuNumero = 20;
+let outroNumero = 15;
+
+console.log(meuNumero + outroNumero);
+console.log(`A soma dos números é: ${meuNumero + outroNumero}`);
+
+meuNumero = 90; //reatribuição de valores
+console.log(`A soma dos números é: ${meuNumero + outroNumero}`);
---
+++ M1/Canvas/sprint-4/03-Atividade-Variaveis/index.js
@@ -0,0 +1,35 @@
+/*1- Faça um programa que exiba um decimal, um inteiro e uma string utilizando o console.log() */
+let decimal = 3.14;
+let inteiro = 7;
+let string = "string";
+
+console.log(decimal, inteiro, string);
+
+/*2- Declare uma variável, atribua um número, use o método typeof para mostrar a palavra na tela "number" */
+let numero = 7;
+console.log(typeof numero);
+
+/*3- Declare uma variável, atribua um valor booleano, use o método typeof para mostrar na tela a palavra "boolean" */
+let aceitaJesus = true;
+console.log(typeof aceitaJesus);
+
+/*4- Declare uma variável, atribua uma string, use o método typeof para mostrar na tela a palavra "string" */
+let palavra = "batata";
+console.log(typeof palavra);
+
+/*5- Faça um programa que exiba duas variáveis, uma com uma atribuição true e a outra false */
+let verdadeiro = true;
+let falso = false;
+
+console.log(verdadeiro, falso);
+
+/*6- Usando uma variável, e o console, mostre o valor null */
+let nulo = null;
+console.log(nulo);
+
+/*7- Usando uma variável e o console, mostre o valor undefined */
+let naoDefinida;
+console.log(naoDefinida);
+
+let n1 = 379825930-54
+console.log(n1)
