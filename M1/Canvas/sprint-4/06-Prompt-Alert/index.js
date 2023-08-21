824c9635273381a4b37f2c2af9d64a1605d50524
Paloma Cristina Faria <palomacfaria@outlook.com>
2023-08-21 17:50:18 -0300

    Prompt, Alert, Booleanos, Operadores de Comparações

---
+++ M1/Canvas/sprint-4/06-Prompt-Alert/index.js
@@ -0,0 +1,8 @@
+let numeroCorreto = 7;
+let palpite = prompt("Adivinhe o número que estou pensando, entre 1 e 10!");
+
+if (palpite == numeroCorreto) {
+    alert("Parabéns! Você acertou o número!");
+} else {
+    alert("Poxa, não é esse número. Tente de novo!");
+}
---
+++ M1/Canvas/sprint-4/07-Booleanos-Comparacoes/index.js
@@ -0,0 +1,25 @@
+let idadeJoao = 22;
+let idadeMaria = 15;
+
+let joaoPodeEntrar = idadeJoao > 18;
+console.log(joaoPodeEntrar);
+
+let mariaPodeEntrar = idadeMaria > 18;
+console.log(mariaPodeEntrar);
+
+
+//Operadores de igualdade 
+console.log(1 == "1"); // Saída: true
+console.log(1 === "1"); // Saída: false
+
+//Operadores de não igualdade
+console.log(1 != "1"); // Saída: false
+console.log(1 !== "1"); // Saída: true
+
+/*FALSY
+false
+undefined
+null
+""
+0
+NaN*/
