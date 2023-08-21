f659cb2a78c7aab2585eeb807bba82653f77398d
Paloma Cristina Faria <palomacfaria@outlook.com>
2023-08-21 19:37:59 -0300

    Demo de introdução ao JavaScript

---
+++ M1/Demos/sprint-4/01-Introducao/index.js
@@ -0,0 +1,33 @@
+console.log("Hello Word!");
+console.log("Paloma");
+
+//Variável mutavel (Pode ser alterada) - (Variável local)
+let nome = "Paloma";
+console.log(nome);
+
+//Reatribuição de Valor
+nome = "Vanda";
+console.log(nome);
+
+//Variável constante (Não pode ser alterada)
+const dataNascimento = "08/02/2000";
+console.log(dataNascimento);
+
+//Variável mutavel (Pode ser alterada) - (Variável global)
+//var idade = 23;
+idade = 25;
+console.log(idade);
+
+//Tipos de dados
+const numero = 10; //Number Integer
+console.log(numero);
+
+const numero2 = 19.8; //Number float
+console.log(numero2);
+
+const texto = "Js é muito bom!"; //String
+console.log(texto);
+
+const verdadeiro = true; //Boolean
+const falso = false;
+console.log(verdadeiro, falso);
