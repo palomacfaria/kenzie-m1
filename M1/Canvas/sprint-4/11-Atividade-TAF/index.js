dc996bbd4b881a6d0cb4292a45edb3d41f407ddf
Paloma Cristina Faria <palomacfaria@outlook.com>
2023-08-24 01:17:15 -0300

    Atividade TAF correção

--- M1/Canvas/sprint-4/11-Atividade-TAF/index.js
+++ M1/Canvas/sprint-4/11-Atividade-TAF/index.js
@@ -16,23 +16,25 @@ let result = false;
 //Desenvolva seu código aqui:
 if(gender == "male"){
     if(height >= 1.7 && (barReps >= 6 || barSeconds >= 15) && abs >= 41 &&
-        (runDistance >= 4000 && 900) && (swimDistance >= 100 && (swimTime >= 60 || diveTime >= 30))) {
+        ((runDistance >= 3000 && runTime <= 720) || (runDistance >= 5000 && runTime <= 1200)) &&
+        (swimDistance >= 100 && (swimTime >= 60 || diveTime <= 30))) {
         result = true;
-        console.log("Passou");
+        console.log("Parabéns, você foi aprovado na TAF");
     }
     else{
         result = false;
-        console.log("Não passou");
+        console.log("Sinto muito, quem sabe na próxima vez...");
     }
 } else if(gender == "female"){
-    if(height >= 1.6 && (barReps >= 5 || barSeconds >= 12) && abs >= 41 && 
-        (runDistance >= 4000 && 900) && (swimDistance >= 100 && (swimTime >= 60 || diveTime >= 30))) {
-            result = true;
-            console.log("Passou");
+    if(height >= 1.6 && (barReps >= 5 || barSeconds >= 12) && abs >= 41 &&
+        ((runDistance >= 4000 && runTime <= 900) || (runDistance >= 6000 && runTime <= 1320)) &&
+        (swimDistance >= 100 && (swimTime >= 60 || diveTime <= 30))) {
+        result = true;
+        console.log("Parabéns, você foi aprovado na TAF");
     }
     else{
         result = false;
-        console.log("Não passou");
+        console.log("Sinto muito, quem sabe na próxima vez...");
     }
 }
         
