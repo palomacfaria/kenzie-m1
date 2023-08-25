88d22981b3fe2ff122f36472b7c8ecca82376425
Paloma Cristina Faria <palomacfaria@outlook.com>
2023-08-25 17:58:37 -0300

    Introdução as funções

---
+++ M1/Canvas/sprint-5/01-Funcoes/index.js
@@ -0,0 +1,24 @@
+//Funções
+
+// - Organizar um bloco de código em um conjunto fechado
+// - Permitir que o código seja dividido em partes menores
+// - Reutilização de código
+
+/* const nome = "Thiago";
+const sobrenome = "Koman";
+const modulo = "M1";
+
+console.log(`O nome completo do instrutor é ${nome} ${sobrenome}, e ele trabalha no módulo ${modulo}.`) */
+
+function formataApresentacao() {
+  const nome = "Thiago";
+  const sobrenome = "Koman";
+  const modulo = "M1";
+
+  console.log(`O nome completo do instrutor é ${nome} ${sobrenome}, e ele trabalha no módulo ${modulo}.`);
+}
+
+formataApresentacao();
+formataApresentacao();
+formataApresentacao();
+formataApresentacao();
