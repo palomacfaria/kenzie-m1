62564546024e2c4e48d5c9310639f3b74310eefb
Paloma Cristina Faria <palomacfaria@outlook.com>
2023-09-14 14:25:46 -0300

    Finalizando Atividade Loteria

--- M1/Canvas/sprint-7/08-Atividade-Loteria-Premiada/index.js
+++ M1/Canvas/sprint-7/08-Atividade-Loteria-Premiada/index.js
@@ -1,17 +1,19 @@
 /* Exercício 1 - Pessoa Premiada
 Você está trabalhando na loteria federal e um cliente se aproxima e fala que foi premiado.
 
-Os dados do ganhador já estão no sistema e você agora só deve comparar se as informações prestadas pelo suposto ganhador são realmente verdadeiras. Pegue os documentos necessários e faça a consulta. */
+Os dados do ganhador já estão no sistema e você agora só deve comparar se as informações prestadas pelo suposto ganhador são realmente verdadeiras. Pegue os documentos necessários e faça a consulta. 
+- Crie uma função que irá receber o nome e cpf do suposto ganhador.
+Dentro da função você deverá comparar os dados recebidos com os dados existentes.
+A função deve retornar uma string: "É ganhador!" ou "Não é ganhador!".
+Desenvolva uma lógica para que o nome sempre tenha a primeira letra maiúscula e as restantes minúsculas.*/
+ 
 let ganhador = {
     nome: 'Adamastor',
     nascimento: '16/09/1986',
     cpf: '123.321.789-98',
     estadoCivil: 'Casado'
 }
-/*Crie uma função que irá receber o nome e cpf do suposto ganhador.
-Dentro da função você deverá comparar os dados recebidos com os dados existentes.
-A função deve retornar uma string: "É ganhador!" ou "Não é ganhador!".
-Desenvolva uma lógica para que o nome sempre tenha a primeira letra maiúscula e as restantes minúsculas. */
+
 function eGanhador(nome, cpf){
   //Transformando nome
   let inicial = nome[0].toUpperCase();
@@ -43,32 +45,31 @@ Com base no exercício anterior, insira todos que não ganharam o prêmio em um 
 Crie uma função que recebe um objeto.
 Crie um array e insira o objeto passado.
 Retorne uma string contendo o número de pessoas que vieram atrás do prêmio, mas não eram ganhadores. */
-let enganador1 = {
+
+let naoGanhador = {
   nome: 'Francisco',
   nascimento: '20/02/1953',
   cpf: '056.879.456-12',
   estadoCivil: 'Viuvo'
 }
-let enganador2 = {
-  nome: 'Katrina',
-  nascimento: '04/11/1998',
-  cpf: '456.125.457-10',
+let naoGanhador2 = {
+  nome: 'Natalia',
+  nascimento: '15/12/1995',
+  cpf: '475.561.456-12',
   estadoCivil: 'Solteira'
 }
-let enganador3 = {
-  nome: 'Marli',
-  nascimento: '28/04/1965',
-  cpf: '222.789.951-98',
-  estadoCivil: 'Casada'
-}
-let enganador4 = {
-  nome: 'Flávio',
-  nascimento: '16/09/1986',
-  cpf: '323.379.153-58',
-  estadoCivil: 'Casado'
-}
 
-function quemVeioAtrasDoPremio(nomes){
-  let enganadores = [];
+let listaNaoGanhadores = [];
+
+function quemVeioAtrasDoPremio(pessoa){
+
+  if(eGanhador(pessoa.nome, pessoa.cpf) === 'Não é ganhador!'){
+    listaNaoGanhadores.push(pessoa);
+  }
+
+  return `Vieram atrás do premio ${listaNaoGanhadores.length} pessoa(s).`;
+
 }
-console.log(quemVeioAtrasDoPremio())
+console.log(quemVeioAtrasDoPremio(naoGanhador));
+console.log(quemVeioAtrasDoPremio(naoGanhador2));
+console.log(quemVeioAtrasDoPremio(ganhador));
